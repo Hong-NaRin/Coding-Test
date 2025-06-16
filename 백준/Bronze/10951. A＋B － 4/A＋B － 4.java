1109d6190064d2a9415adc15a274dff04790e373
narin <hnr8652@gmail.com>
2025-06-16 20:55:43 +0900

    [Bronze V] Title: A+B - 4, Time: 224 ms, Memory: 17852 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// 백준 - A+B-4 10951번
+		Scanner scan = new Scanner(System.in);
+	
+		while(scan.hasNext()) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			System.out.println(a+b);
+		}
+    }
+}
