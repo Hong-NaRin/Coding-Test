bcf200bbecb3454b8e6d3418d3a1fefeb8a8bb15
narin <hnr8652@gmail.com>
2025-06-16 19:59:23 +0900

    [level 0] Title: 특수문자 출력하기, Time: 139.86 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+        // ->  \(역슬래시), '(홑따옴표), "(더블쿼터)를 출력하기 위해서는 앞에 \(역슬래시) 를 붙임
+        // \'를 출력하기 위해서는 앞에 \하나만 붙여서 \\'를 만들어 주면 됨 
+    }
+}
