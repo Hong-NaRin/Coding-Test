9308616520d25b39a2647a4cb34e2b9fa1c60260
narin <hnr8652@gmail.com>
2025-06-16 19:37:11 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.05 ms, Memory: 68.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] num, int n) {
+        
+        int[] arr = new int[n];
+        for(int i =0; i < n; i++) {
+            arr[i] = num[i];
+        }
+        return arr;
+    }
+}
