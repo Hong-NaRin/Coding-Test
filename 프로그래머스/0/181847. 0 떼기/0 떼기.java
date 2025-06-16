b24b4448f80f744d3337ff74a757dc930f297733
narin <hnr8652@gmail.com>
2025-06-16 19:30:59 +0900

    [level 0] Title: 0 떼기, Time: 8.10 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String n_str) {
+        return "" + Integer.parseInt(n_str);
+        // int cnt = 0;
+        // for(int i = 0; i < n_str.length(); i++) {
+        //     if(n_str.charAt(i) != '0' ) break;
+        //     else cnt++;
+        // }
+        // return n_str.substring(cnt);
+    }
+}
