009e95db4e0b3e88cffcf8a25b16b766f68eb3d4
narin <hnr8652@gmail.com>
2025-06-16 20:54:39 +0900

    [Bronze V] Title: 새싹, Time: 112 ms, Memory: 13948 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {  // 클래스명 반드시 Main 
+	
+	public static void main(String[] args) {
+		
+		// 백준 - 새싹 25083번
+		System.out.println("         ,r'\"7\n"
+				+ "r`-_   ,'  ,/\n"
+				+ " \\. \". L_r'\n"
+				+ "   `~\\/\n"
+				+ "      |\n"
+				+ "      |");
+    }
+}
