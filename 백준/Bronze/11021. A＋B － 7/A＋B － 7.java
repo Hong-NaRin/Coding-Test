ddc32a4eb4a23d176e9af07589ddddf22062e975
narin <hnr8652@gmail.com>
2025-06-16 20:59:52 +0900

    [Bronze V] Title: A+B - 7, Time: 216 ms, Memory: 18604 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 백준 - A+B-7 11021번
+		Scanner scan = new Scanner(System.in);
+		int T = scan.nextInt();
+		for(int i = 1; i <= T; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			System.out.println("Case #"+i+": " + (a+b));
+		}
+    }
+}
