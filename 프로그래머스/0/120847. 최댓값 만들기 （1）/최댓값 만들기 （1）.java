c31fb6842e3c8b1dc170669ee0baeac115be8ad2
narin <hnr8652@gmail.com>
2025-06-16 19:07:46 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 1.20 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        int max = numbers[numbers.length-1];
+        int max2 = numbers[numbers.length-2];
+      
+        return max * max2;
+    }
+}
