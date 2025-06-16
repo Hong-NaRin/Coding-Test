42266a55bfb962892cb6830ece44358de58c9636
narin <hnr8652@gmail.com>
2025-06-16 20:08:20 +0900

    [level 0] Title: 외계어 사전, Time: 0.14 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        int answer = 2;
+        for(String d : dic) {
+            int cnt = 0;
+            for(String s : spell) {
+                if(d.contains(s)) cnt++;
+            }
+            if(cnt == spell.length) {
+                answer = 1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
