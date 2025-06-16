9cde68e9c8bc223c77f7d57639952c22feab5769
narin <hnr8652@gmail.com>
2025-06-16 20:55:53 +0900

    [Bronze V] Title: A+B - 5, Time: 188 ms, Memory: 17916 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// 백준 - A+B-4 10951번
+		Scanner scan = new Scanner(System.in);
+	
+		while(scan.hasNext()) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			
+			if(a  == 0 && b == 0 ) {
+				break;
+			}
+			System.out.println(a+b);
+		}
+    }
+}
