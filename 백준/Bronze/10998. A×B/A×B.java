610e26fb1f205a5a9351a3405476b25fed56837b
narin <hnr8652@gmail.com>
2025-06-16 20:49:49 +0900

    [Bronze V] Title: A×B, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {  // 클래스명 반드시 Main 
+	
+	public static void main(String[] args) {
+		
+		//두 정수 A와 B를 입력받은 다음, A×B를 출력
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.print(a * b);
+	}
+}
