996033baaa1159b537ea7211e9c60551177d0694
narin <hnr8652@gmail.com>
2025-06-16 19:16:02 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int price) {
+        
+        if(price >= 500000) {
+            price = (int)(price * 0.8);
+        }
+        else if(price >= 300000) {
+            price = (int)(price * 0.9);
+        }
+        else if(price >= 100000) {
+            price = (int)(price * 0.95);
+        }
+        // if(price >= 500000) {
+        //     price -= price * 0.2;
+        // }
+        // else if(price >= 300000 && price < 500000 ) {
+        //     price -= price * 0.1;
+        // }
+        // else if(price >= 100000 && price < 300000 ) {
+        //     price -= price * 0.05;
+        // }
+        return price;
+    }
+}
