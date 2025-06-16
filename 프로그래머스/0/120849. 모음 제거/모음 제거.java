c45b884cc8369573421fdf4305890789d4d9de59
narin <hnr8652@gmail.com>
2025-06-16 19:07:55 +0900

    [level 0] Title: 모음 제거, Time: 0.17 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string) {
+       return my_string.replaceAll("[aeiou]", "");
+        // String answer = my_string.replace("a", "").replace("e", "").replace("i", "").replace("o", "").replace("u", "");
+        // return answer;
+    }
+}
