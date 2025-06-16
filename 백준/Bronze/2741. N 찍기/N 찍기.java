01e57868ff89ad956657277442352f7eb8ad5a9f
narin <hnr8652@gmail.com>
2025-06-16 20:54:28 +0900

    [Bronze V] Title: N 찍기, Time: 1124 ms, Memory: 33396 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {  // 클래스명 반드시 Main 
+	
+	public static void main(String[] args) {
+		
+		// 백준 - N찍기 2741번
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			System.out.println(i);
+		}
+    }
+}
